13cd28f8586ba9c8725f61170b2b449c7c05f955
Mikhail Novikof <9702571@mail.ru>
2019-11-08 23:08:18 +0300

    Рефакторинг test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -4,11 +4,13 @@
 from pages.product_page import ProductPage
 
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+
 def test_guest_can_add_product_to_basket(browser):
     """
     Тест добавления выбранного товара в корзину
     """
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
