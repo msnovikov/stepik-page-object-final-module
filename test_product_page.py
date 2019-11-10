af6d96344b4ac441648345305b143e25b1f492dc
Mikhail Novikof <9702571@mail.ru>
2019-11-10 15:28:05 +0300

    Добавлены тесты перехода на страницу логина со странице продукта

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,8 @@
 Тест-кейсы для страницы продукта
 """
 import pytest
+
+from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
 
@@ -72,3 +74,27 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_success_message_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    """
+    Тест наличия ссылки на странцу логина/регистрации
+    на странице товара
+    """
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    """
+    Тест перехода на страницу логина/регистрации
+    со страницы товара
+    """
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
